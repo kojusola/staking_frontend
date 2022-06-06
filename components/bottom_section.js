9ac063717a2ec899e6b9d3573d8f43bb7ebee6b4
Solomon <adebayosolomon74@gmail.com>
2022-06-06 23:16:01 +0100

    tweaks

--- components/bottom_section.js
+++ components/bottom_section.js
@@ -4,8 +4,11 @@ import React from "react";
 
 const Chart = () => {
     return (
-        <div css={{background: "rgba(0, 0, 0, 0.22)",  padding: "2%", height: "15rem", margin: "2%"}}>
-           Chart
+        <div>
+             <p6 css={{ fontSize: "18px", lineHeight: "25px", color: "#FFF", padding: "2%" }}> Analytics</p6>
+            <div css={{background: "rgba(0, 0, 0, 0.22)",  padding: "2%", height: "15rem", margin: "2%"}}>
+            
+            </div>
         </div>
 
     );
