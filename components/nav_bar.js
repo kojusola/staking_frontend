97d0f48d950eb87f3c38508cc1613476585544a5
Fafemi Adeola <adeola5678@gmail.com>
2022-06-08 15:16:46 +0100

    router updates

--- components/nav_bar.js
+++ components/nav_bar.js
@@ -1,24 +1,40 @@
 /** @jsxImportSource @compiled/react */
 
 import React from "react";
+import { useRouter } from "next/router";
 
 // function LoginLayout() {
-  
-//     let navigate = useNavigate(); 
-//     const routeChange = () =>{ 
-//       let path = `../pages/index.js`; 
+
+//     let navigate = useNavigate();
+//     const routeChange = () =>{
+//       let path = `../pages/index.js`;
 //       navigate(path);
 //     }
-    
 
 const NavBar = () => {
-    return (
-        <div>
-           <p5 css={{ fontSize: "18px", color: "#4E2DC8", marginRight: "80%", padding: "2%"}}> Stako</p5>
-           <button className="connect">Connect Wallet</button>
-        </div>
-
-    );
+  const router = useRouter();
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push("/");
+  };
+  return (
+    <div>
+      <p5
+        css={{
+          fontSize: "18px",
+          color: "#4E2DC8",
+          marginRight: "80%",
+          padding: "2%",
+        }}
+      >
+        {" "}
+        Stako
+      </p5>
+      <button className="connect" onClick={handleClick}>
+        Connect Wallet
+      </button>
+    </div>
+  );
 };
 
 export default NavBar;
